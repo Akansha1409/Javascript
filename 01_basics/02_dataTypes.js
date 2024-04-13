91560d304b346cfddc5cb86172da0fc8ed28a8cb
Akansha <111658205+Akansha1409@users.noreply.github.com>
2024-04-13 10:50:41 +0000

    config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"//treat all js code as newer version
+//alert(3+3) we are using nodejs,not browser
+//number
+//bigint
+//string
+//boolean
+//null (type=object)
+//undefined(type=undefined)
+//symbol
+//object
